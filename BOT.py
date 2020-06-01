0a41c403b1c4182666dd5038279268dc6cfeee68
luihin903 <66258216+luihin903@users.noreply.github.com>
2020-06-01 21:38:29 +0800

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,1 +1,7 @@
-hi
+import discord
+from discord.ext import commands
+bot=commands.Bot(command_prefix='[')
+@bot.event
+async def on_ready():
+    print('Bot is online')
+bot.run('NzE3MDAwNzczMzI1NTUzNzI3.XtT9Xg.iYitMQJNU1dXCBpAmowpHnr5Ek4')
